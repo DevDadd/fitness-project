b058614ee08d41b787fb518423ec1e39ba2f1b20
DevDad123 <anhnq.b23dt013@stu.ptit.edu.vn>
2026-01-26 22:14:25 +0700

    Update pubspec to include salomon_bottom_bar dependency and add new icons to assets. Refactor main.dart to set HomePageCore as the initial route.

---
+++ lib/feature/workout/presentation/pages/workoutlist_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WorkoutlistPage extends StatelessWidget {
+  const WorkoutlistPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(child: Text('Workout List')),
+    );
+  }
+}
